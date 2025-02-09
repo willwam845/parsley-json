4db2352c4661f72c97a3df31b57eb5e93ff1a2b4
willwam <willwam845@gmail.com>
2025-02-09 01:33:48 +0000

    feat: add main parser program

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -1,5 +1,25 @@
 package json
 
+import scala.io.Source
+import java.io.File
+import parsley.Success
+import parsley.Failure
+
 def main(args: Array[String]): Unit = {
-    println("hello world!")
+    args.headOption match {
+        case Some(filename) => {
+            val file = File(filename)
+            if (!file.exists()) {
+                println("File does not exist!")
+                sys.exit(-1)
+            }
+            val prog = Source.fromFile(file).getLines().mkString("\n")
+            parser.parse(prog) match {
+                case Success(x) => println(x)
+                case Failure(msg) => print(msg)
+            }
+        }
+        case None => println("Please provide a json file.")
+    }
+
 }
