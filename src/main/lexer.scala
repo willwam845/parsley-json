7e32631b760a6b93a15f897269cb37f3fe4eff0d
willwam <willwam845@gmail.com>
2025-02-09 01:34:49 +0000

    feat: add lexer configuration

--- src/main/lexer.scala
+++ src/main/lexer.scala
@@ -3,12 +3,42 @@ package json
 import parsley.Parsley
 import parsley.token.Lexer
 import parsley.token.descriptions.*
+import parsley.token.Unicode
 
 object lexer {
-    private val desc = LexicalDesc.plain.copy()
+    private val whitespace: Set[Int] = Set('\u0020', '\u000a', '\u000d', '\u0009')
+
+    private val desc = LexicalDesc.plain.copy(
+        spaceDesc = SpaceDesc.plain.copy(
+            space = Unicode(whitespace.contains(_))
+        ),
+        textDesc = TextDesc.plain.copy(
+            characterLiteralEnd = '\'',
+            stringEnds = Set(("\"", "\"")),
+            escapeSequences = EscapeDesc.plain.copy(
+                escBegin = '\\',
+                literals = Set('\"', '\\', '/'),
+                mapping = Map(
+                    "b" -> '\b',
+                    "f" -> '\f',
+                    "n" -> '\n',
+                    "r" -> '\r',
+                    "t" -> '\t'
+                ) // TODO: handle unicode escapes of the form \uXXXX
+            )
+        ),
+        numericDesc = NumericDesc.plain.copy(
+            leadingZerosAllowed = false,
+            leadingDotAllowed = false,
+            trailingDotAllowed = false,
+            integerNumbersCanBeBinary = false,
+            integerNumbersCanBeOctal = false,
+            integerNumbersCanBeHexadecimal = false
+        )
+    )
     val lexer = Lexer(desc)
     val implicits = lexer.lexeme.symbol.implicits
-    val string = lexer.lexeme.string.ascii
+    val string = lexer.lexeme.string.fullUtf16
     val integer = lexer.lexeme.integer.decimal
     val float = lexer.lexeme.floating.number
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
